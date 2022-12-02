a3c4e28a8ac85088f4c4707ddf3045a11c52da95
shadynitesh22 <nitesh.poudel@hotmail.com>
2022-12-02 12:42:09 +0545

    Bootstrap-- Using bootstrap via npm --

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { LearnAngularComponent } from './learn-angular/learn-angular.component';
 import { CliDocComponent } from './cli-doc/cli-doc.component';
 import { AngularMaterialComponent } from './angular-material/angular-material.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
@@ -18,8 +21,10 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
